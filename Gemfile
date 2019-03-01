84b7ebc2c0a385dbe55f3a3cf67af2fedada7a56
Brian Bensky <brianbensky@gmail.com>
2019-03-01 13:30:14 -0700

    updated circle conifg, Gemfile , Rakefile, added tests

--- Gemfile
+++ Gemfile
@@ -1,2 +1,8 @@
 source 'http://rubygems.org'
+
 gem 'sinatra'
+gem 'minitest'
+gem 'minitest-ci'
+gem 'rack'
+gem 'rack-test'
+gem 'rake'
---
+++ test/test_helloworld.rb
@@ -0,0 +1,19 @@
+require_relative '../helloworld.rb'
+require 'minitest/autorun'
+require 'minitest/ci'
+require 'rack/test'
+
+class HelloWorldTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_hello_world
+    get '/'
+
+    assert last_response.ok?
+    assert_includes last_response.body, "Hello"
+  end
+end
